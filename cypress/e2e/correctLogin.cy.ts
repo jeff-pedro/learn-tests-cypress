4efab36009b4160b31528aa4282db2a1633202b9
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-07 23:06:19 -0300

    Refactory Login page for a happy path 🥳

--- cypress/e2e/correctLogin.cy.ts
+++ cypress/e2e/correctLogin.cy.ts
@@ -1,6 +1,9 @@
 describe('Login page', () => {
-    it('Should fill out a login form correctly', () => {
-        cy.visit('http://localhost:4200/#/home')
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/')
+    })
+
+    it('Should fill out a login form correctly and log in', () => {
         cy.get('[data-test="loginUserName"]').type('sparrow')
         cy.get('[data-test="loginPassword"]').type('theblackpearl')
         cy.contains('button', 'login').click()
