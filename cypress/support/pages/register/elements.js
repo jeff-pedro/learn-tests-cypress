f956df84aeffe547f1ea584c7ff0d5b215db4113
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-10 19:08:09 -0300

    Struture follow the Page Object Model pattern

---
+++ cypress/support/pages/register/elements.js
@@ -0,0 +1,8 @@
+export const ELEMENTS = {
+    registerNow: '[data-test="register"]',
+    email: '[data-test="email"]',
+    fullName: '[data-test="fullName"]',
+    username: '[data-test="registerUserName"]',
+    password: '[data-test="registerPassword"]',
+    btnRegister: '[data-test="btnRegister"]'
+}
