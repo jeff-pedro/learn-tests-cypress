6bbfe7dd17401042d0da126a7552557eb37f0d3f
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-10 13:25:48 -0300

    Simulate flaky test

--- cypress/e2e/apiAlurapic.cy.ts
+++ cypress/e2e/apiAlurapic.cy.ts
@@ -12,7 +12,21 @@ describe('Get pictures and data', () => {
         })
     })
 
-    it('Get one user picture', () => {
+    // it('Get user pictures', () => {
+    //     cy.request({
+    //         method: 'GET',
+    //         url: 'http://localhost:3000/sparrow/photos?page=1'
+    //     }).then((res) => {
+    //         expect(res.status).to.be.equal(200)
+    //         expect(res.body).is.not.empty
+    //         expect(res.body[0]).to.have.property('description')
+    //         expect(res.body[0].description).to.be.equal('fiona')
+    //     })
+    // })
+
+    it('Get user pictures (simulating flaky test)', () => {
+        const expectTime = Math.random() * 50  // simula tempo esperado para carregar página 
+
         cy.request({
             method: 'GET',
             url: 'http://localhost:3000/sparrow/photos?page=1'
@@ -21,6 +35,7 @@ describe('Get pictures and data', () => {
             expect(res.body).is.not.empty
             expect(res.body[0]).to.have.property('description')
             expect(res.body[0].description).to.be.equal('fiona')
+            expect(res.duration).to.be.lte(expectTime)
         })
     })
 })
