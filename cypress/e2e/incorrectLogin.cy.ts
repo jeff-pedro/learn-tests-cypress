216b8457f1df78ee7ab64a073bbca7045ada0023
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-10 10:22:19 -0300

    Add stub to simulate invalid password or username

--- cypress/e2e/incorrectLogin.cy.ts
+++ cypress/e2e/incorrectLogin.cy.ts
@@ -1,7 +1,20 @@
 describe('Login page', () => {
-    it('Verify message of required fields', () => {
+    
+    beforeEach(() => {
         cy.visit('http://localhost:4200/#/home')
+
+        cy.intercept('POST', 'http://localhost:3000/user/login', {
+            statusCode: 401
+        }).as('stubPost')
+    })
+    
+    it('Verify message of required fields', () => {
         cy.contains('User name is required!').should('be.visible')
         cy.contains('Password is required!').should('be.visible')
+    })
+
+    it('Should fill out a form with incorrect login values', () => {
+        cy.login('sparrow', '123456')
+        cy.wait('@stubPost')
     })
 })
